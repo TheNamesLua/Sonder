b1240afdf7b1b4d85ca71d81b55b7df6a17862b4
TheNamesLua <78677238+TheNamesLua@users.noreply.github.com>
2023-08-29 06:34:26 -0400

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,4 @@
-	--!nocheck
+--!nocheck
 
 	--[[
 		// File Name: Sonder.lua
@@ -17,110 +17,108 @@
 		sonder:GetItems() -- returns a table of every item currently in the sonder
 	]]
 
-	local sonder = {}
-	sonder.ClassName = "Sonder"
+local sonder = {}
+sonder.ClassName = "Sonder"
 
-	export type _SonderClass = {
-		Handle : (self, thing_or_things: any)->nil,
-		Release: (self, thing: any)->nil,
-		Clean : (self)->nil,
-		Destroy : (self)->nil,
-		Supports: (self, thing: any)->boolean,
-		Extends: (self)->_SonderClass,
-		GetItems: (self)->any,
-	}
+export type _SonderClass = {
+	Handle : (self, thing_or_things: any)->nil,
+	Release: (self, thing: any)->nil,
+	Clean : (self)->nil,
+	Destroy : (self)->nil,
+	Supports: (self, thing: any)->boolean,
+	Extends: (self)->_SonderClass,
+	GetItems: (self)->any,
+}
 
-	local Supported_Types = {
-		"Instance",
-		"RBXScriptConnection",
-		"thread",
-		"table",
-	}
+local Supported_Types = {
+	"Instance",
+	"RBXScriptConnection",
+	"thread",
+	"table",
+}
 
-	local function CleanupItem(item)
-		if not item then
-			return
+local function CleanupItem(item)
+	if not item then
+		return
+	end
+
+	local type = typeof(item)
+
+	local passed, err = pcall(function()
+		if type == "Instance" then
+			item:Destroy()
 		end
-		
-		local type = typeof(item)
 
-		local passed, err = pcall(function()
-			if type == "Instance" then
+		if type == "RBXScriptConnection" then
+			if item.Connected then
+				item:Disconnect()
+			end
+		end
+
+		if type == "thread" then
+			coroutine.close(item)
+		end
+
+		if type == "table" then
+			if item.Destroy then
 				item:Destroy()
-			end
-			
-			if type == "RBXScriptConnection" then
-				if item.Connected then
-					item:Disconnect()
-				end
-			end
-			
-			if type == "thread" then
-				coroutine.close(item)
-			end
-				
-			if type == "table" then
-				if item.Destroy then
-					item:Destroy()
-				else
-					error("Error cleaning up table", 2)
-				end	
-			end
-			
-			error(`Could not clean up item {item}`)
-		end)
-
-		if not passed then
-			error(err)
+			else
+				error("Error cleaning up table", 2)
+			end	
 		end
+	end)
+
+	if not passed then
+		error(err)
+	end
+end
+
+function sonder.new(): _SonderClass
+	return setmetatable(sonder, sonder)
+end
+
+function sonder:Handle(...)
+	for _, item in ipairs(table.pack(...)) do
+		local _type = typeof(item)
+		assert(table.find(Supported_Types, _type), `Type {_type} Is not supported with sonder. List of supported items: {table.unpack(Supported_Types)}`)
+
+		table.insert(self.Items, item)
+	end
+end
+
+function sonder:Release(item: any)
+	local exists = table.find(self.Items, item)
+
+	if exists then
+		table.remove(self.Items, exists)
+	end
+end
+
+function sonder:Supports(item)
+	if table.find(Supported_Types, typeof(item)) then
+		return true
 	end
 
-	function sonder.new(): _SonderClass
-		return setmetatable(sonder, sonder)
+	return false
+end
+
+function sonder:Clean()
+	for index, item in ipairs(self.Items) do
+		CleanupItem(item)
 	end
 
-	function sonder:Handle(...)
-		for _, item in ipairs(table.pack(...)) do
-			local _type = typeof(item)
-			assert(table.find(Supported_Types, _type), `Type {_type} Is not supported with sonder. List of supported items: {table.unpack(Supported_Types)}`)
+	table.clear(self.Items)
+end
 
-			table.insert(self.Items, item)
-		end
-	end
+function sonder:GetItems(): {any}
+	return self.Items
+end
 
-	function sonder:Release(item: any)
-		local exists = table.find(self.Items, item)
-		
-		if exists then
-			table.remove(self.Items, exists)
-		end
-	end
+sonder.Items = {}
 
-	function sonder:Supports(item)
-		if table.find(Supported_Types, typeof(item)) then
-			return true
-		end
-		
-		return false
-	end
+--@alias for sonder:Clean()
+sonder.Destroy = sonder.Clean
+--@alias for sonder.new
+sonder.Extends = sonder.new
 
-	function sonder:Clean()
-		for index, item in ipairs(self.Items) do
-			CleanupItem(item)
-		end
-		
-		table.clear(self.Items)
-	end
-
-	function sonder:GetItems(): {any}
-		return self.Items
-	end
-
-	sonder.Items = {}
-
-	--@alias for sonder:Clean()
-	sonder.Destroy = sonder.Clean
-	--@alias for sonder.new
-	sonder.Extends = sonder.new
-
-	return sonder
+return sonder
