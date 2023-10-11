67231c3f8fff6ecea8ae4b2a220863c4c5d26818
TheNamesLua <78677238+TheNamesLua@users.noreply.github.com>
2023-10-11 19:21:36 -0400

    Update Source.lua
    removed unused variables

--- Source.lua
+++ Source.lua
@@ -17,111 +17,111 @@
 		sonder:GetItems() -- returns a table of every item currently in the sonder
 	]]
 
-local sonder = {}
-sonder.ClassName = "Sonder"
-
-export type _SonderClass = {
-	Handle : (self, thing_or_things: any)->nil,
-	Release: (self, thing: any)->nil,
-	Clean : (self)->nil,
-	Destroy : (self)->nil,
-	Supports: (self, thing: any)->boolean,
-	Extends: (self)->_SonderClass,
-	GetItems: (self)->any,
-}
-
-local Supported_Types = {
-	"Instance",
-	"RBXScriptConnection",
-	"thread",
-	"table",
-}
-
-local function CleanupItem(item)
-	if not item then
-		return
-	end
-
-	local type = typeof(item)
-
-	local passed, err = pcall(function()
-		if type == "Instance" then
-			item:Destroy()
-		end
-
-		if type == "RBXScriptConnection" then
-			if item.Connected then
-				item:Disconnect()
-			end
-		end
-
-		if type == "thread" then
-			coroutine.close(item)
-		end
-
-		if type == "table" then
-			if item.Destroy then
-				item:Destroy()
-			else
-				error("Error cleaning up table", 2)
-			end	
-		end
-	end)
-end
-
-function sonder.new(): _SonderClass
-	local newSonder = {}
-	newSonder.ClassName = "Sonder"
-	newSonder.Items = {} -- Initialize the Items table for the new object
-
-	for key, value in pairs(sonder) do
-		if type(value) == "function" then
-			newSonder[key] = value
-		end
-	end
-
-	setmetatable(newSonder, sonder)
-	return newSonder
-end
-
-function sonder:Handle(...)
-	for _, item in ipairs(table.pack(...)) do
-		local _type = typeof(item)
-		assert(table.find(Supported_Types, _type), `Type {_type} Is not supported with sonder. List of supported items: {table.unpack(Supported_Types)}`)
-
-		table.insert(self.Items, item)
-	end
-end
-
-function sonder:Release(item: any)
-	local exists = table.find(self.Items, item)
-
-	if exists then
-		table.remove(self.Items, exists)
-	end
-end
-
-function sonder:Supports(item)
-	return table.find(Supported_Types, typeof(item)) ~= nil
-end
-
-function sonder:Clean()
-	for index, item in ipairs(self.Items) do
-		CleanupItem(item)
-	end
-
-	table.clear(self.Items)
-end
-
-function sonder:GetItems(): {any}
-	return self.Items
-end
-
-sonder.Items = {}
-
---@alias for sonder:Clean()
-sonder.Destroy = sonder.Clean
---@alias for sonder.new
-sonder.Extends = sonder.new
-
-return sonder
+    local sonder = {}
+    sonder.ClassName = "Sonder"
+    
+    export type _SonderClass = {
+        Handle : (self, thing_or_things: any)->nil,
+        Release: (self, thing: any)->nil,
+        Clean : (self)->nil,
+        Destroy : (self)->nil,
+        Supports: (self, thing: any)->boolean,
+        Extends: (self)->_SonderClass,
+        GetItems: (self)->any,
+    }
+    
+    local Supported_Types = {
+        "Instance",
+        "RBXScriptConnection",
+        "thread",
+        "table",
+    }
+    
+    local function CleanupItem(item)
+        if not item then
+            return
+        end
+    
+        local type = typeof(item)
+    
+        pcall(function()
+            if type == "Instance" then
+                item:Destroy()
+            end
+    
+            if type == "RBXScriptConnection" then
+                if item.Connected then
+                    item:Disconnect()
+                end
+            end
+    
+            if type == "thread" then
+                coroutine.close(item)
+            end
+    
+            if type == "table" then
+                if item.Destroy then
+                    item:Destroy()
+                else
+                    error("Error cleaning up table", 2)
+                end	
+            end
+        end)
+    end
+    
+    function sonder.new(): _SonderClass
+        local newSonder = {}
+        newSonder.ClassName = "Sonder"
+        newSonder.Items = {} -- Initialize the Items table for the new object
+    
+        for key, value in pairs(sonder) do
+            if type(value) == "function" then
+                newSonder[key] = value
+            end
+        end
+    
+        setmetatable(newSonder, sonder)
+        return newSonder
+    end
+    
+    function sonder:Handle(...)
+        for _, item in ipairs(table.pack(...)) do
+            local _type = typeof(item)
+            assert(table.find(Supported_Types, _type), `Type {_type} Is not supported with sonder. List of supported items: {table.unpack(Supported_Types)}`)
+    
+            table.insert(self.Items, item)
+        end
+    end
+    
+    function sonder:Release(item: any)
+        local exists = table.find(self.Items, item)
+    
+        if exists then
+            table.remove(self.Items, exists)
+        end
+    end
+    
+    function sonder:Supports(item)
+        return table.find(Supported_Types, typeof(item)) ~= nil
+    end
+    
+    function sonder:Clean()
+        for _, item in ipairs(self.Items) do
+            CleanupItem(item)
+        end
+    
+        table.clear(self.Items)
+    end
+    
+    function sonder:GetItems(): {any}
+        return self.Items
+    end
+    
+    sonder.Items = {}
+    
+    --@alias for sonder:Clean()
+    sonder.Destroy = sonder.Clean
+    --@alias for sonder.new
+    sonder.Extends = sonder.new
+    
+    return sonder
